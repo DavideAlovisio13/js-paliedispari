0830b7ec4bd8c334d3f422c1e8ee6461a25fd054
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-14 15:38:17 +0100

    (js) add for and create condition

--- js/script.js
+++ js/script.js
@@ -6,14 +6,31 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 // dichiaro le mie variabili contenenti gli elementi html
 const inputBtn = document.getElementById("input_btn");
-// genero il mio contenitore vuoto 
+// genero i miei contenitori vuoti: uno lineare e un altro invertito
 let arrayWords = [];
+let revArrayWords = [];
 // aggiungo un evento al click del pulsante 
 inputBtn.addEventListener('click', function(){
     //estrapolo il valore dal mio input su html
     const inputText = document.getElementById("input_text").value;
+    //inserisco il valore all'interno dei miei due contenitori, divindendo le varie lettere. Nell'ultimo invertirò l'ordine
     arrayWords.push(inputText.split(''));
+    revArrayWords.push(inputText.split('').reverse());
+    // a quetso punto creo un ciclo dove vado a ....
+    for(let i = 0; i < arrayWords.length && revArrayWords.length; i++){
+        //....prima di tutto riunire gli elementi all'interno degli array....
+        let arrayWordsCheck = arrayWords[i].join('');
+        console.log(arrayWordsCheck);
+        let revArrayWordsCheck = revArrayWords[i].join('');
+        console.log(revArrayWordsCheck);
+        //....poi a verificare sei due oggetti composti siano identici oppure no
+        if (arrayWordsCheck !== revArrayWordsCheck) {
+            let
+        }
+    }
+
     console.log(arrayWords);
+    console.log(revArrayWords);
 
   
     // genero la mia funzione per controllare se la parola è palindroma oppure no
