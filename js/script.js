81a4545b1f05467590ab91f6e9f7b521cfa3d841
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-14 16:03:29 +0100

    (js) finish ex 1

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 // dichiaro le mie variabili contenenti gli elementi html
 const inputBtn = document.getElementById("input_btn");
-// genero i miei contenitori vuoti: uno lineare e un altro invertito
-let arrayWords = [];
-let revArrayWords = [];
+let outputText = document.getElementById('result');
 // aggiungo un evento al click del pulsante 
-inputBtn.addEventListener('click', function(){
+inputBtn.addEventListener('click', function () {
+    // resetto il mio output
+    outputText.innerText ="";
+    // genero i miei contenitori vuoti: uno lineare e un altro invertito
+    let arrayWords = [];
+    let revArrayWords = [];
     //estrapolo il valore dal mio input su html
     const inputText = document.getElementById("input_text").value;
     //inserisco il valore all'interno dei miei due contenitori, divindendo le varie lettere. Nell'ultimo invertirò l'ordine
     arrayWords.push(inputText.split(''));
     revArrayWords.push(inputText.split('').reverse());
     // a quetso punto creo un ciclo dove vado a ....
-    for(let i = 0; i < arrayWords.length && revArrayWords.length; i++){
+    for (let i = 0; i < arrayWords.length && revArrayWords.length; i++) {
         //....prima di tutto riunire gli elementi all'interno degli array....
         let arrayWordsCheck = arrayWords[i].join('');
         console.log(arrayWordsCheck);
@@ -25,18 +28,12 @@ inputBtn.addEventListener('click', function(){
         console.log(revArrayWordsCheck);
         //....poi a verificare sei due oggetti composti siano identici oppure no
         if (arrayWordsCheck !== revArrayWordsCheck) {
-            let
+            outputText.innerText = 'purtroppo la parola inserita non è palindroma';
+        } else {
+            outputText.innerText = 'la tua parola è palindorma!';
         }
     }
-
-    console.log(arrayWords);
-    console.log(revArrayWords);
-
-  
-    // genero la mia funzione per controllare se la parola è palindroma oppure no
-   
-    
-    
-    
 })
+
+// esercizio 2
 
