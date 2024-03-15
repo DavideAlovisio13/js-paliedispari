fb4be8803ca9e9aa0b15fa9f7a970b7fbfb1293b
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-15 11:11:29 +0100

    (js) correct exercises

--- js/script.js
+++ js/script.js
@@ -4,36 +4,52 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// dichiaro le mie variabili contenenti gli elementi html
-const inputBtn = document.getElementById("input_btn");
-let outputText = document.getElementById('result');
-// aggiungo un evento al click del pulsante 
-inputBtn.addEventListener('click', function () {
-    // resetto il mio output
-    outputText.innerText = "";
-    // genero i miei contenitori vuoti: uno lineare e un altro invertito
-    let arrayWords = [];
-    let revArrayWords = [];
-    //estrapolo il valore dal mio input su html
-    let inputText = document.getElementById("input_text").value;
-    //inserisco il valore all'interno dei miei due contenitori, divindendo le varie lettere. Nell'ultimo invertirò l'ordine
-    arrayWords.push(inputText.split(''));
-    revArrayWords.push(inputText.split('').reverse());
-    // a quetso punto creo un ciclo dove vado a ....
-    for (let i = 0; i < arrayWords.length && revArrayWords.length; i++) {
-        //....prima di tutto riunire gli elementi all'interno degli array....
-        let arrayWordsCheck = arrayWords[i].join('');
-        console.log(arrayWordsCheck);
-        let revArrayWordsCheck = revArrayWords[i].join('');
-        console.log(revArrayWordsCheck);
-        //....poi a verificare sei due oggetti composti siano identici oppure no
-        if (arrayWordsCheck !== revArrayWordsCheck) {
-            outputText.innerText = 'purtroppo la parola inserita non è palindroma';
-        } else {
-            outputText.innerText = 'la tua parola è palindorma!';
-        }
+// // dichiaro le mie variabili contenenti gli elementi html
+// const inputBtn = document.getElementById("input_btn");
+// let outputText = document.getElementById('result');
+// // aggiungo un evento al click del pulsante 
+// inputBtn.addEventListener('click', function () {
+//     // resetto il mio output
+//     outputText.innerText = "";
+//     // genero i miei contenitori vuoti: uno lineare e un altro invertito
+//     let arrayWords = [];
+//     let revArrayWords = [];
+//     //estrapolo il valore dal mio input su html
+//     let inputText = document.getElementById("input_text").value;
+//     //inserisco il valore all'interno dei miei due contenitori, divindendo le varie lettere. Nell'ultimo invertirò l'ordine
+//     arrayWords.push(inputText.split(''));
+//     revArrayWords.push(inputText.split('').reverse());
+//     // a quetso punto creo un ciclo dove vado a ....
+//     for (let i = 0; i < arrayWords.length && revArrayWords.length; i++) {
+//         //....prima di tutto riunire gli elementi all'interno degli array....
+//         let arrayWordsCheck = arrayWords[i].join('');
+//         console.log(arrayWordsCheck);
+//         let revArrayWordsCheck = revArrayWords[i].join('');
+//         console.log(revArrayWordsCheck);
+//         //....poi a verificare sei due oggetti composti siano identici oppure no
+//         if (arrayWordsCheck !== revArrayWordsCheck) {
+//             outputText.innerText = 'purtroppo la parola inserita non è palindroma';
+//         } else {
+//             outputText.innerText = 'la tua parola è palindorma!';
+//         }
+//     }
+// })
+
+const userWord = prompt('Type a word')
+
+function isPalindroma(word) {
+    let reverseWord = '';
+
+    for (let index = word.length - 1; index >= 0; index--) {
+        reverseWord += word[index];
     }
-})
+
+    if (reverseWord === word) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // esercizio 2
 
@@ -57,18 +73,27 @@ Dichiariamo chi ha vinto.
 // scrivo se ha vinto oppure no
 
 // condizioni da verificare
+const userChoise = prompt('scrivi pari o dispari');
+const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
+
+const alNumber = getRndInteger(1, 5);
+
+let result = oddEven(userChoise + userNumber);
+
+if (result === userChoise) {
+    console.log('hai vinto!')
+} else {
+    console.log('ho vinto io!!!')
+}
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
-  const userEven= bottone
-  const userOdd = bottone
-  const userNum = input
-  const compNum = getRndInteger(1,5);
-  let sum = userEven + userOdd
-
-  function EvenOrOdd () {
-    
-  }
+function oddEven(number) {
+    if (number % 2 === 0)
+        return 'even';
+    } else {
+    return 'odd';
+}
 
